1b6980de9571ea87b01090cc6aa7fd14bb955a42
Sahil Sanghvi <sanghviss@berkeley.edu>
2019-12-27 13:03:43 -0800

    wip

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+//for arrow click part (PART2)
+$(document).ready( function () {
+ $('#down').on('click',function () {
+  $("html").scrollTop(0);
+   $('html, body').animate({
+     scrollTop: $("#after_landing").offset().top
+ }, 1000);
+ });
+});
